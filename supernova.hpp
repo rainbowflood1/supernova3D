d4d42b379e259aa56e00a8f4830de2641fc5c63a
Izzedin <izzy.asad@gmail.com>
2026-06-28 16:50:16 -0400

    Included GLFW, and GLAD, and added game class.
    The game class is used for window creation, and draw loops.

--- supernova.hpp
+++ supernova.hpp
@@ -1,7 +1,15 @@
 #ifndef SUPERNOVA
 #define SUPERNOVA
 
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
 
+class Game {
+public:
+	Game() { // Initalize GLFW and create window
+		
+	}
+};
 
 
 #endif
