b8df76abd2d21883cc8faf7d0def337bccfdf134
Izzedin <izzy.asad@gmail.com>
2026-06-29 00:29:29 -0400

    Added the mesh class and the Vector3 class
    Made the shaders functional

--- examples/game.cpp
+++ examples/game.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
 #include "../supernova.hpp"
 
-
-
-
+Shaders shader;
+Mesh mesh;
 
 void update(double dt) {
+	shader.UseShaders();
+	mesh.Render();
 	//std::cout << dt << std::endl;
 }
 
 int main() {
-	Game game(800, 600, "Game");
-	Shaders shader;
+	mesh.Verts = {
+		Vector3(-0.5, 0, 0),
+		Vector3(0.5, 0, 0),
+		Vector3(0, 0.5, 0),
+		Vector3(0.5, 0.5, 0)
+	};
 	
-	shader.ShaderSourceFiles("./examples/vertex_shader.glsl", "./examples/fragment_shader.glsl");
+	mesh.Ind = {
+		0, 1, 2,
+		3, 2, 1
+	};
+
+	Game game(800, 600, "Game");
+
+	shader.CreateShaders();
+	
+	mesh.Init();
 	
 	game.Update(&update);
 	
 	shader.UnloadShaders();
 
-	game.Terminate();
-	std::cout << shader.vertex_shader_source << std::endl;
+	game.Terminate();	
+	
 	return 0;
 }
