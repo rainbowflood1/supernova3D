17101e53b395b1307cbe6962ac1afecb79823a4f
Izzedin <izzy.asad@gmail.com>
2026-06-29 00:27:44 -0400

    Added the mesh class

--- supernova.hpp
+++ supernova.hpp
@@ -5,18 +5,44 @@
 #include <string>
 #include <functional>
 #include <fstream>
+#include <vector>
 
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+#define TRIANGLES GL_TRIANGLES
+
+
 class Shaders {
 public:
 	std::string vertex_shader_source = "#version 440 core\nlayout (location = 0) in vec3 aPos;\nvoid main() {\n\tgl_Position = vec4(aPos.x, aPos.y, aPos.z, 1);\n}";
-	std::string fragment_shader_source = "#version 440 core\nout vec3 col;\nvoid main() {\n\tcol = vec3(1, 1, 1)\n}";
+	std::string fragment_shader_source = "#version 440 core\nout vec3 col;\nvoid main() {\n\tcol = vec3(1, 1, 1);\n}";
 
-	GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
-	GLuint program = glCreateProgram();
+	GLuint vshader;
+	GLuint fshader;
+	GLuint program;
+
+	void CreateShaders() {
+		const char* vertex_shader_const_char = this->vertex_shader_source.c_str();
+		const char* fragment_shader_const_char = this->fragment_shader_source.c_str();
+		// Because it is an std::string, it will be converted to a const char* by using std::string::c_str()
+		
+		// Create Shaders
+		this->vshader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(this->vshader, 1, &vertex_shader_const_char, NULL);
+		glCompileShader(this->vshader);
+
+		this->fshader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(this->fshader, 1, &fragment_shader_const_char, NULL);
+		glCompileShader(this->fshader);
+
+		// Attach shaders to the program
+		
+		this->program = glCreateProgram();
+		glAttachShader(this->program, this->vshader);
+		glAttachShader(this->program, this->fshader);
+		glLinkProgram(this->program);
+	}
 	
 	void ShaderSourceFiles(std::string vertex_shader_file, std::string fragment_shader_file) {
 		std::ifstream vshaderfile(vertex_shader_file);
@@ -46,6 +72,10 @@ public:
 		fshaderfile.close();
 	}
 
+	void UseShaders() {
+		glUseProgram(this->program);
+	}
+
 	void UnloadShaders() {
 		glDeleteShader(this->vshader);
 		glDeleteShader(this->fshader);
@@ -54,6 +84,84 @@ public:
 
 	
 	
+};
+
+class Vector3 {
+public:
+	GLfloat x;
+	GLfloat y;
+	GLfloat z;
+	Vector3(GLfloat x = 0.0, GLfloat y = 0.0, GLfloat z = 0.0) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+};
+
+class Mesh {
+public:
+	int mode = TRIANGLES;
+	std::vector<Vector3> Verts;
+	std::vector<int> Ind;
+	void Init() {
+		//GLfloat* points = this->VecToFloat().data();
+		//int* indices = this->Ind.data();
+
+		size_t points_size = this->Verts.size()*sizeof(Vector3);	
+
+		GLfloat points[points_size];
+		
+		for (int i = 0; i < points_size; i++) {
+			points[i] = this->VecToFloat().data()[i];
+		}	
+
+
+		size_t indices_size = this->Ind.size();
+
+		int indices[indices_size];
+
+		for (int i = 0; i < indices_size; i++) {
+			indices[i] = this->Ind[i];
+		}
+
+
+		// Initalize buffers
+		glGenVertexArrays(1, &this->VAO);
+		glGenBuffers(1, &this->VBO);
+		glGenBuffers(1, &this->EBO);
+
+		glBindVertexArray(this->VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+	}
+
+	void Render() {
+		glBindVertexArray(this->VAO);
+		glDrawElements(this->mode, this->Ind.size(), GL_UNSIGNED_INT, 0);
+	}
+private:
+	GLuint VBO, VAO, EBO;
+
+	std::vector<GLfloat> VecToFloat() {
+		size_t points_size = this->Verts.size()*sizeof(Vector3);	
+		std::vector<GLfloat> vector_points;
+
+		for (Vector3 vec : this->Verts) {
+			vector_points.push_back(vec.x);
+			vector_points.push_back(vec.y);
+			vector_points.push_back(vec.z);
+		}
+
+		return vector_points;
+	}
 };
 
 class Game {
@@ -90,16 +198,17 @@ public:
 		double last_time = 0.0;
 		double dt = 0.0;
 
+		glViewport(0, 0, this->WindowWidth, this->WindowHeight);
+
 		while (!glfwWindowShouldClose(this->window)) {
 			dt = glfwGetTime()-last_time;
-			glfwPollEvents();
-			glViewport(0, 0, this->WindowWidth, this->WindowHeight);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 			glClearColor(0, 0.1, 1, 1);
-			update_function(dt);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
+			update_function(dt);
 			
 			
+			glfwPollEvents();
 			glfwSwapBuffers(this->window);
 			last_time = glfwGetTime();
 		}
